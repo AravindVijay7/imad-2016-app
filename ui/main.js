b13023803df189b52304c371811cf750ec0d00ea
AravindVijay7 <aravindvijay07@gmail.com>
2016-10-23 11:29:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var img= document.getElementById('im');
+var img= document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
