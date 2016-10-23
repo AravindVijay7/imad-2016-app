afa93eaa13ff7236a9ac38a65c842a37e50d93af
AravindVijay <aravindvijay07@gmail.com>
2016-10-23 21:15:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onClick = function(){
      
  };
   
- request.open('GET' 'http://http://aravindvijay7.imad.hasura-app.io/counter', true);
+ request.open('GET','http://http://aravindvijay7.imad.hasura-app.io/counter', true);
  request.send(null);
     
 };
