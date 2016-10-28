e7d44db21a7775332fe10157b5e0db03ec1a5e16
AravindVijay <aravindvijay07@gmail.com>
2016-10-28 18:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,15 +23,15 @@ button.onclick = function(){
 };
 
 
-//name input
- var nameInput = document.getElementById('name');
- var name = nameInput.value;
- var submit = document.getElementById('submit_btn');
- submit.onclick  = fuction(){
-     
-     
- 
- request.onreadystatechange = function(){
+
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+
+     var request = new XMLHttpRequest(); 
+  request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE){
          if(request.status === 200){
              var names = ['name1','name2','name3'];
@@ -51,5 +51,6 @@ button.onclick = function(){
  request.open('GET','http://aravindvijay7.imad.hasura-app.io/submit-name?name=' + name, true);
  request.send(null);
     
-    
-}
+
+   
+};
