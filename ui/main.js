128b902f30d34d354c89739f3e5214737a63f5dc
AravindVijay <aravindvijay07@gmail.com>
2016-10-28 14:27:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,26 +2,26 @@
 var button = document.getElementById("counter");
 
 button.onclick = function(){
-    var counter =0;
-// var request = new XMLHttpRequest();
+    
+ var request = new XMLHttpRequest();
  
  
- //request.onreadystatechange = function(){
-    // if(request.readystate == XMLHttpRequest.DONE){
-         //if(request.status == 200){
-             //var counter = request.responseText;
-             counter = counter + 1;
+ request.onreadystatechange = function(){
+    if(request.readystate == XMLHttpRequest.DONE){
+         if(request.status == 200){
+             var counter = request.responseText;
+            
              var span = document.getElementById('count');
              span.innerHTML = counter.toString();
-         //}
-     //}
+         }
+     }
      
  };
   
- //request.open('GET','http://aravindvijay7.imad.hasura-app.io/counter', true);
- //request.send(null);
+ request.open('GET','http://aravindvijay7.imad.hasura-app.io/counter', true);
+ request.send(null);
     
-//};
+};
 
 //name ip
 
