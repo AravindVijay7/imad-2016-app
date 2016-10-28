b1543468731828025455a34c966dd5925a3f57aa
AravindVijay <aravindvijay07@gmail.com>
2016-10-28 23:03:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user:'aravindvijay7',
+    database:'aravindvijay7',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
