f1dfab29a0340fea273a894a8df51d51bbd00381
AravindVijay <aravindvijay07@gmail.com>
2016-10-28 23:25:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ return htmlTemplate;
 }
 
 app.get('/articles/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
+    //var articleName = req.params.articleName;
     pool.query("SELECT * FROM article WHERE title =" + req,params.articleNmae,function(err, result){
        if(err){
            res,status(500).send(err.toString());
