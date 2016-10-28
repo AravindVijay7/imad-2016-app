d4981e1afd749b5ef8ac6df5ccd396edc50702ac
AravindVijay <aravindvijay07@gmail.com>
2016-10-28 17:57:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,15 +23,33 @@ button.onclick = function(){
 };
 
 
-
-
-
-//name ip
-
-//var nameInput = document.getElementById('name');
-// var name = nameInput.value;
-//var submit = document.getElementById('submit_btn');
-//submit.onClick = fuction(){
+//name input
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick  = fuction(){
+     
+     
+ 
+ request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+         if(request.status === 200){
+             var names = ['name1','name2','name3'];
+             var list = '';
+             for(var i = 0 ; i<names.length; i++){
+                 var ul = document.getElementById('namelist');
+                 ul.innerHTML = list;
+             }
+             
+             
+             
+         }
+     }
+     
+ };
+  
+ request.open('GET','http://aravindvijay7.imad.hasura-app.io/submit-name?name=' + name, true);
+ request.send(null);
     
     
-//};
+}
