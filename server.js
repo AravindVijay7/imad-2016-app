19cdfa221293c7cdd73b1ae23d704e75d6d72f10
AravindVijay <aravindvijay07@gmail.com>
2016-11-05 10:55:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var cfrrypto = require('crypto');
 
 var config = {
     user:'aravindvijay7',
@@ -24,6 +25,22 @@ app.get('/counter',function(req, res){
    counter = counter + 1;
    res.send(counter.toString());
 });
+
+app.get('/hash/:input',function(req,res){
+    
+    var hashString = hash(req.params.input,'this-is-a-ramdom-string');
+    res.sendFile(path.join(_dirname,'ui','index.html'));
+});
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toStrin('hex');
+    
+}
+
+
+
+
 
 app.get('/info', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'info.html'));
