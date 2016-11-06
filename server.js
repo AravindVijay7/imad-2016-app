fef4dcbacd8d21b4f0e1dfe5c5537b480fd2d78e
AravindVijay <aravindvijay07@gmail.com>
2016-11-06 17:51:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,13 @@ app.post('/login',function(req,res){
 
 });
 
+app.get('/check-login',function(req,res){
+    if (req.session && req.session.auth && req.session.auth.userId) {
+    
+    
+    
+    }
+});
 
 
 app.get('/info', function (req, res) {
