9b20591ec7e362c4ffdf71766522d26255b5e57c
AravindVijay <aravindvijay07@gmail.com>
2016-11-06 19:07:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/login',function(req,res){
                if(hashedPassword == dbString){
                    req.session.auth={userId: result.rows[0].id};
                    res.send('LOGIN SUCCESSFUL');
-                   document.location  = "/info";
+                    window.location.href  = '/info';
                }else{
                  res.status(403).send('Username/Password Invalid');  
                }
