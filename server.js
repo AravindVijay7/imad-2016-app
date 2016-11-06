085b564b6ff7c0d4ca577797b5ca5e4c0aa257bc
AravindVijay <aravindvijay07@gmail.com>
2016-11-06 15:28:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+   secret:'someRandomValue',
+   cookie:{maxAge: 1000*60*24*30}
+}));
+
 
 
 app.get('/', function (req, res) {
