3e913c74324f2ffc1d852a397b1823db796c8e9a
AravindVijay <aravindvijay07@gmail.com>
2016-11-07 21:47:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/bootstrap.min.css ', function (req, res) {
+app.get('/ui/bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
 });
 
