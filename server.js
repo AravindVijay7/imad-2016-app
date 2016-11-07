370209b2c7396a356c0c0809f9c31ee2041c5ae6
AravindVijay <aravindvijay07@gmail.com>
2016-11-07 18:52:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -230,9 +230,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/info.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'info.js'));
 });
+
 
 
 app.get('/articles/:articleName', function (req, res) {
