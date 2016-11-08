21f8093b446797c6d2e5cbcd3604827fe8690a9d
AravindVijay <aravindvijay07@gmail.com>
2016-11-08 19:51:48 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,95 +1,51 @@
 
 var currentArticleTitle = window.location.pathname.split('/')[2];
 
-
-
 function loadCommentForm () {
-
     var commentFormHtml = `
-
-        <h5>Submit a comment</h5>
-
+       <h5>Submit a comment</h5>
        <font color="black"> <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea></font>
-
         <br/>
-
        <font color="black"> <input type="submit" id="submit" value="Submit" /> </font>
-
-        <br/>
-
+       <br/>
         `;
-
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
-
-    var submit = document.getElementById('submit');
-
-    submit.onclick = function () {
-
-        var request = new XMLHttpRequest();
-
-        request.onreadystatechange = function () {
-
-          if (request.readyState === XMLHttpRequest.DONE) {
-
-
-                if (request.status === 200) {
-
-                    document.getElementById('comment_text').value = '';
-
-                    loadComments();    
-
-                } else {
-
-                    alert('Error! Could not submit comment');
-
-                }
-
-                submit.value = 'Submit';
-
-          }
-
-        };
-
-
-
-        var comment = document.getElementById('comment_text').value;
-
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
-
-        request.setRequestHeader('Content-Type', 'application/json');
-
-        request.send(JSON.stringify({comment: comment}));  
-
-        submit.value = 'Submitting...';
-
         
-
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    var submit = document.getElementById('submit');
+    submit.onclick = function () {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    document.getElementById('comment_text').value = '';
+                    loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
+          }
+        };
+        
+        var comment = document.getElementById('comment_text').value;
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
     };
-
 }
 
 
 
 function loadLogin () {
-
-
     var request = new XMLHttpRequest();
-
     request.onreadystatechange = function () {
-
         if (request.readyState === XMLHttpRequest.DONE) {
-
             if (request.status === 200) {
-
                 loadCommentForm(this.responseText);
-
             }
-
         }
-
     };
 
-    
 
     request.open('GET', '/check-login', true);
 
@@ -102,64 +58,37 @@ function loadLogin () {
 function escapeHTML (text)
 
 {
-
     var $text = document.createTextNode(text);
-
     var $div = document.createElement('div');
-
     $div.appendChild($text);
-
     return $div.innerHTML;
-
 }
 
 
 
 function loadComments () {
 
-
     var request = new XMLHttpRequest();
-
     request.onreadystatechange = function () {
-
         if (request.readyState === XMLHttpRequest.DONE) {
-
             var comments = document.getElementById('comments');
-
             if (request.status === 200) {
-
                 var content = '';
-
                 var commentsData = JSON.parse(this.responseText);
-
                 for (var i=0; i< commentsData.length; i++) {
-
                     var time = new Date(commentsData[i].timestamp);
-
                     content += `<div class="comment">
-
                         <p>${escapeHTML(commentsData[i].comment)}</p>
-
                         <div class="commenter">
-
                             ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-
                         </div>
-
                     </div>`;
-
                 }
-
                 comments.innerHTML = content;
-
             } else {
-
                 comments.innerHTML('Oops! Could not load comments!');
-
             }
-
         }
-
     };
 
     
@@ -170,11 +99,6 @@ function loadComments () {
 
 }
 
-
-
-
-
-// The first thing to do is to check if the user is logged in!
 
 loadLogin();
 
